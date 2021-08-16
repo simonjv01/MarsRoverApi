613e8c826e4616323ecc299e8d17fc9eac22b8e8
Simon Vargas <sjvargas01@gmail.com>
2021-08-15 22:55:01 -0400

    first commit

---
+++ src/test/java/com/mars/marsroverapi/MarsRoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.marsroverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
