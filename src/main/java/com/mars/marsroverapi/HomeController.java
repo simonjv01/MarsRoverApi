1f95ddc570b1f4782178566b5f72f85432cd0d08
Simon Vargas <sjvargas01@gmail.com>
2021-08-17 22:54:27 -0400

    updated index and homecontroller

--- src/main/java/com/mars/marsroverapi/HomeController.java
+++ src/main/java/com/mars/marsroverapi/HomeController.java
@@ -1,13 +1,15 @@
 package com.mars.marsroverapi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getHomeView() {
+    public String getHomeView(ModelMap model) {
+        model.put("name", "Simon Vargas");
         return "index";
     }
 
