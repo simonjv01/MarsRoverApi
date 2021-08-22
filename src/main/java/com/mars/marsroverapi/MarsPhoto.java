e9caa715d1989adb324ef048e5a6ec3af86146c9
Simon Vargas <sjvargas01@gmail.com>
2021-08-21 23:33:06 -0400

    created three new classes MarsCamera, MarsPhoto, and MarsRoverApiResponse

---
+++ src/main/java/com/mars/marsroverapi/MarsPhoto.java
@@ -0,0 +1,7 @@
+package com.mars.marsroverapi;
+
+public class MarsPhoto {
+    private Long id;
+    private Integer sol;
+    private MarsCamera camera;
+}
---
+++ src/main/java/com/mars/marsroverapi/MarsRoverApiResponse.java
@@ -0,0 +1,8 @@
+package com.mars.marsroverapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarsRoverApiResponse {
+    List<MarsPhoto> photos = new ArrayList<>();
+}
